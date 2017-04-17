e186ed7a0bf6ceef442a99d2e5596eac804311ed
Matt Wiseman <mrslwiseman@gmail.com>
2017-04-17 20:20:14 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let express         = require('express'),
+    mongoose        = require('mongoose')
+    bodyParser      = require('body-parser'),
+    app             = express();
+
+
+// title
+// image
+// body
+// date created
+
+
+
+
+// set up routes
+
+// set up listener
+app.listen
